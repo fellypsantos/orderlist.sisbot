27425dc509f22e2bb121cbd48b20d3dc00ab03ae
Fellyp Santos <fellypsantos2011@gmail.com>
2021-08-20 13:58:27 -0300

    New buttons preview.

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,7 +1,13 @@
 import React, {useContext} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons';
+import {
+  faBicycle,
+  faEye,
+  faEyeSlash,
+  faMale,
+} from '@fortawesome/free-solid-svg-icons';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 import TableOrdersMenu from '../../components/TableOrdersMenu';
 import FormAddOrderItem from '../../components/FormAddOrderItem';
@@ -24,20 +30,31 @@ const Main = () => {
         className="d-flex"
         style={{justifyContent: 'space-between', alignItems: 'center'}}>
         <Title text={Translator('MAIN_TITLE')} />
-        <Button
-          variant="secondary"
-          size="sm"
-          onClick={() => setShowDashboard(!showDashboard)}>
-          <FontAwesomeIcon
-            icon={showDashboard ? faEyeSlash : faEye}
-            size={35}
-          />
-          <span className="ml-1 d-none d-md-inline-block">
-            {showDashboard
-              ? Translator('DASHBOARD_BUTTON_HIDE')
-              : Translator('DASHBOARD_BUTTON_SHOW')}
-          </span>
-        </Button>
+        <div>
+          <ButtonGroup aria-label="Basic example" size="sm" className="mr-1">
+            <Button variant="secondary" style={{width: 35}}>
+              <FontAwesomeIcon icon={faMale} size={35} />
+            </Button>
+
+            <Button variant="secondary" style={{width: 35}}>
+              <FontAwesomeIcon icon={faBicycle} size={35} />
+            </Button>
+          </ButtonGroup>
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => setShowDashboard(!showDashboard)}>
+            <FontAwesomeIcon
+              icon={showDashboard ? faEyeSlash : faEye}
+              size={35}
+            />
+            <span className="ml-1 d-none d-md-inline-block">
+              {showDashboard
+                ? Translator('DASHBOARD_BUTTON_HIDE')
+                : Translator('DASHBOARD_BUTTON_SHOW')}
+            </span>
+          </Button>
+        </div>
       </div>
       <DashboardReports isVisible={showDashboard} />
       <TableOrdersMenu />
