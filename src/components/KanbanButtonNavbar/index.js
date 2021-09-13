1b538c8226979f5f5cdff8e18254f0d091ac8e70
Fellyp Santos <fellypsantos2011@gmail.com>
2021-09-13 14:02:49 -0300

    Preparing kanban project manager.

--- src/components/KanbanButtonNavbar/index.js
+++ src/components/KanbanButtonNavbar/index.js
@@ -3,10 +3,16 @@ import Button from 'react-bootstrap/Button';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faThLarge} from '@fortawesome/free-solid-svg-icons';
 
-const KanbanButtonNavbar = () => (
-  <Button onClick={() => null}>
-    <FontAwesomeIcon icon={faThLarge} />
-  </Button>
-);
+const KanbanButtonNavbar = () => {
+  const OpenKanBan = () => {
+    window.location.href = '/kanban';
+  };
+
+  return (
+    <Button onClick={() => OpenKanBan()}>
+      <FontAwesomeIcon icon={faThLarge} />
+    </Button>
+  );
+};
 
 export default KanbanButtonNavbar;
